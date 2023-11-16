404929153d42b846a5472ab9e6cca63ce0811649
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-11-15 23:02:41 -0700

    this demonstrates relationships between different classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,105 @@
 class Game:
     def __init__(self, title):
-        self.title = title
+        self.__ininit = True;
+        self.setTitle(title);
+        self.__ininit = False;
+
+    def setTitle(self, val):
+        #print(f"self.__ininit = {self.__ininit}");
+        if (self.__ininit):
+            if (type(val) == str and len(val) > 0):
+                self.__title = val;
+    
+    def getTitle(self):
+        return "" + self.__title;
+
+    title = property(getTitle, setTitle);
 
     def results(self):
-        pass
+        return [res for res in Result.all if (res.game == self)];
 
     def players(self):
-        pass
+        return list(set([res.player for res in Result.all if (res.game == self)]));
 
     def average_score(self, player):
-        pass
+        scores = [res.score for res in player.results()];
+        return float(sum(scores))/len(scores);
 
 class Player:
     def __init__(self, username):
-        self.username = username
+        self.setUsername(username);
+
+    def getUsername(self): return self._username;
+
+    def setUsername(self, val):
+        if (type(val) == str and len(val) > 1 and len(val) < 16):
+            self._username = val;
+    
+    username = property(getUsername, setUsername);
 
     def results(self):
-        pass
+        return [res for res in Result.all if (res.player == self)];
+
+    def all_games_played(self):
+        return [res.game for res in Result.all if (res.player == self)];
 
     def games_played(self):
-        pass
+        return list(set(self.all_games_played()));
 
     def played_game(self, game):
-        pass
+        return (self in game.players());
 
     def num_times_played(self, game):
-        pass
+        return sum([1 for res in self.all_games_played() if (res == game)]);
 
 class Result:
+    all = [];
+
     def __init__(self, player, game, score):
-        self.player = player
-        self.game = game
-        self.score = score
+        self.__ininit = True;
+        self.setPlayer(player);
+        self.setGame(game);
+        self.setScore(score);
+        self.__ininit = False;
+        Result.all.append(self);
+    
+    def getScore(self): return self.__score;
+
+    def setScore(self, val):
+        if (self.__ininit):
+            if ((type(val) == int or type(val) == float) and (val > 1 or val == 1) and
+                (val < 5000 or val == 5000)):
+                    self.__score = val;
+
+    score = property(getScore, setScore);
+
+    def setPlayer(self, val):
+        if (self.__ininit):
+            if (type(val) == Player):
+                self.__player = val;
+
+    def getPlayer(self): return self.__player;
+
+    player = property(getPlayer, setPlayer);
+
+    def setGame(self, val):
+        if (self.__ininit):
+            if (type(val) == Game):
+                self.__game = val;
+
+    def getGame(self): return self.__game;
+
+    game = property(getGame, setGame);
+
+
+
+
+#mygame = Game("mytitle");
+#print(mygame.__ininit);
+#attribute error, but can define one, that is different than it so the title never gets changed
+#print("attempting to change the title now!");
+#mygame.setTitle("nwtitle");
+#mygame.title = "nwtitle";
+#print(mygame.title);
+#assert(mygame.title == "mytitle");
+#print("the title did not change!");
